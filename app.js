27b1a339e79e2595c7f80a5d906724ddb0e97197
Jeremy Morgan <jeremymorgan@gmail.com>
2015-03-20 00:35:31 -0700

    then is not a function.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('githubbattle', []);
 
-    var MainController = function($scope, $http) {
+    var MainController = function ($scope, $http) {
 
         $scope.player1input = "DannyNunez";
         $scope.player2input = "JeremyMorgan";
@@ -11,102 +11,123 @@
         $scope.player2 = null;
 
 
-        var getPlayer1Stats = function(response) {
+        var getPlayer1Stats = function (response) {
             // get response data (JSON)
             $scope.player1 = response.data;
         };
 
-        var getPlayer2Stats = function(response) {
+        var getPlayer2Stats = function (response) {
             // get response data (JSON)
             $scope.player2 = response.data;
         };
 
 
-        var onError = function(reason) {
+        var onError = function (reason) {
             $scope.error = "Could not fetch user";
         };
 
+        var p1 = new Promise(
 
-        var scrapeGitHub = function (player1input, player2input){
+            function(resolve, reject) {
+
+                $http.get("https://api.github.com/users/" + player1input)
+                    .then(getPlayer1Stats, onError);
+
+                $http.get("https://api.github.com/users/" + player2input)
+                    .then(getPlayer2Stats, onError);
+
+            }
+        );
+
+
+        var scrapeGitHub = function (player1input, player2input) {
+
 
             $http.get("https://api.github.com/users/" + player1input)
                 .then(getPlayer1Stats, onError);
 
             $http.get("https://api.github.com/users/" + player2input)
                 .then(getPlayer2Stats, onError);
+
+            return true;
         };
 
 
-        var compareResults = function(metric){
+        var compareResults = function (metric) {
 
-        switch(metric){
+            switch (metric) {
 
-            case 'publicrepos':
+                case 'publicrepos':
 
-                //console.log("We got here - public repos");
+                    console.log("We got here - public repos");
 
-                var p1metric = $scope.player1.public_repos;
-                var p2metric = $scope.player2.public_repos;
+                    var p1metric = $scope.player1.public_repos;
+                    var p2metric = $scope.player2.public_repos;
 
-                var p1display = $scope.publicrepos1;
-                var p2display = $scope.publicrepos2;
+                    var p1display = $scope.publicrepos1;
+                    var p2display = $scope.publicrepos2;
 
-                break;
+                    break;
 
-            case 'publicgists':
+                case 'publicgists':
 
-                //console.log("We got here - public gists");
+                    //console.log("We got here - public gists");
 
-                var p1metric = $scope.player1.public_gists;
-                var p2metric = $scope.player2.public_gists;
-                var p1display = $scope.publicgists1;
-                var p2display = $scope.publicgists2;
+                    var p1metric = $scope.player1.public_gists;
+                    var p2metric = $scope.player2.public_gists;
+                    var p1display = $scope.publicgists1;
+                    var p2display = $scope.publicgists2;
 
-                break;
-            case 'followers':
+                    break;
+                case 'followers':
 
-                //console.log("We got here - followers");
+                    //console.log("We got here - followers");
 
-                var p1metric = $scope.player1.followers;
-                var p2metric = $scope.player2.followers;
-                var p1display = $scope.followers1;
-                var p2display = $scope.followers2;
+                    var p1metric = $scope.player1.followers;
+                    var p2metric = $scope.player2.followers;
+                    var p1display = $scope.followers1;
+                    var p2display = $scope.followers2;
 
-                break;
+                    break;
 
-        }
+            }
 
-        //console.log("Output: " + metric);
+            //console.log("Output: " + metric);
 
-        // highlight the higher number in green, lower in red
+            // highlight the higher number in green, lower in red
 
-            if(p1metric == p2metric){
+            if (p1metric == p2metric) {
 
                 p1display = "bg-warning";
                 p2display = "bg-warning";
 
-            }else {
-                if(p1metric > p2metric){
+            } else {
+                if (p1metric > p2metric) {
                     p1display = "bg-success";
                     p2display = "bg-danger";
 
-                    }else {
-                        p1display = "bg-danger";
-                        p2display = "bg-success";
-                    }
+                } else {
+                    p1display = "bg-danger";
+                    p2display = "bg-success";
+                }
             }
 
         };
 
-        $scope.search = function(player1input, player2input) {
-
-
-
-            scrapeGitHub(player1input,player2input);
+        var compareAll = function(){
 
             compareResults("publicrepos");
-            compareResults("publicgists");
-            compareResults("followers");
+            //compareResults("publicgists");
+            //compareResults("followers");
+        };
+
+        $scope.search = function (player1input, player2input) {
+
+        scrapeGitHub(player1input, player2input).then(compareAll,onError);
+
+            //compareResults("publicrepos");
+            //compareResults("publicgists");
+            //compareResults("followers");
 
             //$scope.publicrepos2 = "bg-success";
 
